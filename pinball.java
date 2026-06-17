582d0f629be845bebf20028668c8745047fcf3b5
Drew Osmundson <62411410+drewosmundson@users.noreply.github.com>
2026-06-16 20:52:15 -0500

    Create pinball.java

---
+++ pinball.java
@@ -0,0 +1,57 @@
+
+
+class regions {
+
+
+} 
+
+
+
+
+class Force { 
+
+    
+
+
+} 
+
+
+
+
+class Circle () {
+    int region
+    public Force collision(shape) {
+
+    public void update() {
+        
+
+    }
+
+} 
+
+
+
+
+
+class MathUtils {
+
+
+
+} 
+
+
+class Vector2 { 
+    int x;
+    int y;
+
+    public int distance(Vector2 v1, Vector2 v2) {
+        
+        return v1
+
+        
+    }
+
+
+
+
+} 
